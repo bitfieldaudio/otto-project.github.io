ed2cc4ef9dc54cfa2a24a31f83f719123433ce24
topisani <topisani@hamsterpoison.com>
2019-11-02 03:50:10 +0100

    Updates

--- test/__mocks__/path.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-const path = jest.genMockFromModule('path');
-
-path.resolve = (...pathSegment) =>
-  ['base-path', ...pathSegment].join('/');
-
-module.exports = path;
