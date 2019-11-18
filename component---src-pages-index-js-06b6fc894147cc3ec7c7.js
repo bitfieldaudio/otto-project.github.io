f556b81627b8a97487b4ab7fac1afbf9ba3332ea
Travis CI User <travis@example.org>
2019-11-18 21:45:37 +0000

    Updates

---
+++ component---src-pages-index-js-06b6fc894147cc3ec7c7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{460:function(n,e,a){"use strict";a.r(e);var t=a(0),u=a.n(t),c=a(160),l=a(459);e.default=function(n){return u.a.createElement(l.a,null,u.a.createElement(c.a,null))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-06b6fc894147cc3ec7c7.js.map
