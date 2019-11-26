c7c2666c959f0b6f123fee6ceee35a680637c64d
Travis CI User <travis@example.org>
2019-11-26 15:50:00 +0000

    Updates

---
+++ component---src-templates-redirect-js-1a143798c717954bb464.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{459:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a);t.default=function(e){var t=e.pageContext.to;return r.a.createElement("head",null,r.a.createElement("meta",{"http-equiv":"Refresh",content:"0; url="+t}))}}}]);
+//# sourceMappingURL=component---src-templates-redirect-js-1a143798c717954bb464.js.map
